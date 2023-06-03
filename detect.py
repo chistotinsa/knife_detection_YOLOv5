4620cb9bbe8e17cd4ae7bea93f1120dfe66795f0
Svyatoslav <115224929+chistotinsa@users.noreply.github.com>
2023-06-03 14:30:40 +0300

    remove img resize for Linux in inference
    Signed-off-by: Svyatoslav <115224929+chistotinsa@users.noreply.github.com>

--- detect.py
+++ detect.py
@@ -177,10 +177,6 @@ def run(
             # Stream results
             im0 = annotator.result()
             if view_img:
-                if platform.system() == 'Linux' and p not in windows:
-                    windows.append(p)
-                    cv2.namedWindow(str(p), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
-                    cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
                 cv2.imshow(str(p), im0)
                 cv2.waitKey(1)  # 1 millisecond
 
